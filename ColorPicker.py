20d7c9387fa799263a229e18dff753d1341ce119
EvanPrianto <48738094+EvanPrianto@users.noreply.github.com>
2020-11-06 06:26:45 +0900

    Update ColorPicker.py
    adjust width and height of frame. So have same optimal frame with camera

--- ColorPicker.py
+++ ColorPicker.py
@@ -1,8 +1,8 @@
 import cv2
 import numpy as np
 
-frameWidth = 640
-frameHeight = 480
+frameWidth = 1920
+frameHeight = 1080
 cap = cv2.VideoCapture(0)
 cap.set(3, frameWidth)
 cap.set(4, frameHeight)
